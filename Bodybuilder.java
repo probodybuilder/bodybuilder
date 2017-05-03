119e7bedb3df3ebb46f993d2cdb0b51f675fceab
probodybuilder <aman.thombre@gmail.com>
2017-05-03 11:23:50 -0400

    Update Bodybuilder.java

--- Bodybuilder.java
+++ Bodybuilder.java
@@ -1,3 +1,18 @@
 public class Bodybuilder{
   
+  private int squat;
+  private int bench;
+  private int deadlift;
+  private int overhead; 
+  
+  public Bodybuilder(int sq, int bp, int dl, int ohp){
+    squat = sq;
+    bench = bp;
+    deadlift = dl;
+    overhead = ohp;
+  }
+  
+  public void bodybuild(int val){
+    //todo 
+  }
 }
